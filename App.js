5672da53d3beb9b62ccde8d998db98bff7f944be
Tyler Cupit <t.cupit@abs-link.com>
2024-09-26 10:54:51 -0500

    initial build

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
+
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+
+  const [fontsLoaded, fontError] = useFonts({
+    'Hochstadt-Serif': require('./assets/fonts/Hochstadt-Serif.otf'),
+    'TheGreatOutdoors-Regular': require('./assets/fonts/TheGreatOutdoors-Regular.otf'),
+    'WorkSans-Regular': require('./assets/fonts/WorkSans-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: 'TheGreatOutdoors-Regular', fontSize: 30 }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text>Extra</Text>
       <StatusBar style="auto" />
     </View>
   );
